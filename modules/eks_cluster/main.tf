bcee06094b7bc8eb076ddef3d560f5b982dc0577
mninawa <mninawa@gmail.com>
2024-09-22 21:38:38 +0200

    server

--- modules/eks_cluster/main.tf
+++ modules/eks_cluster/main.tf
@@ -221,8 +221,8 @@ module "eks" {
       instance_types  = ["t3.small"]
 
       min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      max_size     = 2
+      desired_size = 2
       subnet_ids   = data.aws_subnets.private.ids
     }
   }
